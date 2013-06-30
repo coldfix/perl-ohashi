001c4a21b5bbe9f8603a8b72a44f9e53919474d1
Thomas Gläßle <t_glaessle@gmx.de>
2013-06-30 16:47:14 +0200

    some more example code

--- example.pl
+++ example.pl
@@ -7,6 +7,8 @@ use 5.14.0;
 use ohashi;
 use JSON::PP;
 
+
+# This stuff all works as expected:
 my $x = new ohashi(x => 'z', z => 3, alpha => 'alpha');
 $x->{pi} = 3.14;
 
@@ -21,3 +23,27 @@ $x->{beta} = '< 1';
 
 print $x->{bEtA} . "\n";
 print JSON::PP->new->pretty->encode($x);
+
+
+
+# Does not work - empty output: 
+my %y = (
+    alpha => 1,
+    beta  => 2,
+    gamma => 3,
+    delta => 4,
+);
+tie (%y, 'ohash');
+print JSON::PP->new->pretty->encode(\%y);
+
+
+# Works as expected:
+tie (my %z, 'ohash');
+%z = (
+    alpha => 1,
+    beta  => 2,
+    gamma => 3,
+    delta => 4,
+);
+print JSON::PP->new->pretty->encode(\%z);
+
